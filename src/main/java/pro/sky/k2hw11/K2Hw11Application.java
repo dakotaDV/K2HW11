1860cc7d2040bd442b01ce10d749a1a39ee23e10
Prudnikova <dakoto4ka@yandex.ru>
2023-05-31 18:27:47 +0300

    первый коммит

---
+++ src/main/java/pro/sky/k2hw11/K2Hw11Application.java
@@ -0,0 +1,13 @@
+package pro.sky.k2hw11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K2Hw11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K2Hw11Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/k2hw11/K2Hw11ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.k2hw11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K2Hw11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
