4614dc47486bf9884cb3e186e4716a0525812c2a
Prudnikova <dakoto4ka@yandex.ru>
2023-05-31 20:43:14 +0300

    дз сделано

---
+++ src/main/java/pro/sky/k2hw11/repository/StoreRepository.java
@@ -0,0 +1,36 @@
+package pro.sky.k2hw11.repository;
+
+import jakarta.annotation.PostConstruct;
+import pro.sky.k2hw11.model.Item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StoreRepository {
+    private int idCounter = 1;
+    private final Map<Integer, Item>store = new HashMap<>();
+
+    @PostConstruct
+    public void init(){
+        store.put(idCounter, new Item(idCounter++,"Ноутбук"));
+        store.put(idCounter, new Item(idCounter++,"Смартфон"));
+        store.put(idCounter, new Item(idCounter++,"Электронная книга"));
+
+    }
+    public void add(Item item){
+        store.put(item.getId(),item);
+    }
+    public Item get(int id){
+        return store.get(id);
+    }
+    public List<Item> getALL(){
+        return new ArrayList<>(store.values());
+    }
+    public void update(int id, Item item){
+        if(store.containsKey(id)){
+            store.replace(id,item);
+        }
+    }
+}
